94492f06c4576ccc2d7915bf434ca8da1e3d9283
Daniel Gutierrez Montoya <Daniel Gutierrez Montoya>
2016-03-14 17:01:38 -0500

    Se mejoro el algoritmo

---
+++ WebContent/js/ball.js
@@ -0,0 +1,65 @@
+var indexBalls = 0;
+
+function Ball(initpos,speed,radius,startAngle) {
+	this.initPos = initpos;
+	this.context;
+	this.color = "205,110,100"; //defaultColor
+	this.gravity = 1;
+	this.isAlive = false;
+	this.axisSpeed; 
+	this.timeLife = 0;
+	this.name;
+	this.opacity = 1;
+	this.Yspeep = 2;
+	this.speed = speed || 20 ;
+	this.radius=radius || 40;
+	this.startAngle=startAngle || 35;
+	this.direction = {
+		x:1,y:1
+	};
+	
+	this.name = "ball:"+(++indexBalls);
+	
+	this.getAxisSpeed = function (){
+		/*if(this.axisSpeed){
+			return this.axisSpeed;
+		}*/
+		if (!this.speed || !this.startAngle){
+			return undefined;
+		}else{
+			this.axisSpeed = {
+					angle:this.startAngle,
+					speed:this.speed,
+					x : Math.cos(getRadians(this.startAngle))*this.speed, 
+					y : Math.sin(getRadians(this.startAngle))*this.speed 
+				};
+			return this.axisSpeed;
+		}
+	};
+		
+	this.bouncingPoint = {
+		x:this.initPos.x,
+		y:this.initPos.y
+	};	
+	
+	this.start = function start(){
+		this.isAlive = true;
+	};
+	
+	this.changeXdirection = function(){
+		this.direction.x = changeDirection(this.direction.x);
+		this.direction.y = changeDirection(this.direction.y);
+		this.bouncingPoint.y = this.initPos.y;
+		this.bouncingPoint.x = this.initPos.x;
+	}
+	this.changeYdirection = function(){
+		this.direction.y = changeDirection(this.direction.y);
+		this.bouncingPoint.y = this.initPos.y;
+		this.bouncingPoint.x = this.initPos.x;
+	}
+	this.kill = function (){
+		this.isAlive = false;
+	}
+	
+	
+}
--- WebContent/js/follower.js
+++ WebContent/js/follower.js
@@ -1,78 +1,106 @@
 var i = 1;
 var yBouncePoint = 0;
 var xBouncePoint = 0;
+var isStarted = false;
+var ballArray = [];
+var graceTime = 200;
+var canvasWidth = 800;
 
-function Ball(initpos,speed,radius,startAngle) {
-	this.initPos = initpos;
-	this.context;
-	this.speed = speed || 20 ;
-	this.radius=radius || 40;
-	this.startAngle=startAngle || 35;
-	this.direction = {
-		x:1,y:1
-	};
-	
-	this.bouncingPoint = {
-		x:this.initPos.x,
-		y:this.initPos.y
-	}	
-	
-	this.start = function start(ctx){
-		var ball = this;
-		
-	}
-	
-}
+
 
 var settings = {
-		speed : 20 ,
-		r:40,
-		startAngle:35
+		balls: 1,
+		fps:40,
+		startingSpeed : 2
 	}	
 
 
 function startFollow(){
-	 var ball = new Ball({x:100,y:100});
-	 ball.speed=2;
-	 ball.startAngle=/*Math.random()*/45;
-	 
-	 var speed = ball.speed;
-	 var radius = ball.radius;
-	 
-	 var canvas = document.getElementById("canvas");
-	 var localSpeed = ball.speed; 
-	 var pos = [radius+30,0];
-	 var Xdirection = 1;
-	 var Ydirection = Math.tan(getRadians(ball.startAngle));
-	 
-	 canvas.width=window.innerWidth;
-	 canvas.height=window.innerHeight;
-	 canvas.style.display = "block";
-	 var ctx = canvas.getContext("2d");
-	 ball.start(ctx);
+	var color = $("[name=color]:checked").val();
+	var radius = parseInt($("#radius").val());
+	var number = $("#number ").val() || 1;
 	
-	 setInterval(function(){
-			
-		 ctx.clearRect(0, 0, canvas.width, canvas.height);
-		 ctx.fillStyle = "black";
-		 ctx.beginPath();
-		 ball.initPos.x+=(ball.speed*ball.direction.x);
-		 ball.initPos.y =(ball.initPos.x*ball.direction.y)-(ball.bouncingPoint.x*ball.direction.y)+ball.bouncingPoint.y;
-		 ctx.arc(ball.initPos.x,ball.initPos.y,ball.radius,0,(2*Math.PI)/1);
-		 ctx.stroke();
-		 
-		 if(ball.initPos.x+radius>=canvas.width || ball.initPos.x-radius<0){
-			 ball.direction.x = changeDirection(direction.x);
-			 ball.direction.y = changeDirection(direction.y);
-			 ball.bouncingPoint.y = ball.initPos.y;
-			 ball.bouncingPoint.x = ball.initPos.x
-		 }
-		 if(ball.initPos.y+radius>=canvas.height || ball.initPos.y-radius<0){
-			 ball.direction.y = changeDirection(ball.direction.y);
-			 ball.bouncingPoint.y = ball.initPos.y;
-			 ball.bouncingPoint.x = ball.initPos.x
-		 }
-		},1000/35)
+	for (var i = 0; i < number; i++) {
+		var ball = new Ball({
+			x : 50,
+			y : 50
+		}, 100);
+		
+		if(color){
+			ball.color = color;
+		}
+		if(radius){
+			ball.radius = radius;
+		}
+		ball.opacity = 0.4;
+		ball.speed = settings.startingSpeed*(Math.random()+1);
+		ball.startAngle = Math.random() * 90;
+
+		var radius = ball.radius;
+
+		var canvas = document.getElementById("canvas");
+		var Xdirection = 1;
+		var radiansAngle = getRadians(ball.startAngle);
+		ball.direction.y = Math.tan(radiansAngle);
+
+		
+		ball.start();
+		ballArray.push(ball);
+		cleanArray();
+		
+	}
+	 
+	if(!isStarted){
+		isStarted = true;
+		
+		canvas.width = canvasWidth;
+		//canvas.height = window.innerHeight;
+		canvas.style.display = "block";
+		var ctx = canvas.getContext("2d");
+		
+		setInterval(function(){
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			 for (var i = 0;i<ballArray.length;i++){
+				 ball = ballArray[i];
+				 if (ball.isAlive) {
+					ctx.beginPath();
+					
+					ball.initPos.x += ((ball.getAxisSpeed().x) * ball.direction.x);
+					ball.initPos.y = (ball.initPos.x * ball.direction.y)
+							- (ball.bouncingPoint.x * ball.direction.y)
+							+ ball.bouncingPoint.y;
+					
+					ctx.arc(ball.initPos.x, ball.initPos.y, ball.radius, 0,
+							(2 * Math.PI) / 1);
+					ctx.stroke();
+					ctx.fillStyle = "rgba("+ball.color+","
+							+ (ball.opacity -= 0.00002) + ")";
+					ctx.fill();
+					
+					
+					/*ctx.font = "15px Arial";
+					ctx.fillStyle="black";
+					ctx.fillText(ball.name, ball.initPos.x-10, ball.initPos.y);*/
+					
+					
+					if (ball.initPos.x + ball.radius >= canvas.width || ball.initPos.x - ball.radius < 0) {
+						ball.changeXdirection();
+						ball.radius -= 0.2;
+					}
+					if (ball.initPos.y + ball.radius >= canvas.height|| ball.initPos.y - ball.radius < 0) {
+						ball.changeYdirection();
+						ball.radius -= 0.2;
+					}
+					if (ball.opacity <= 0) {
+						ball.kill();
+					}
+					 ball.timeLife++;
+				}
+				 validateBallImpact(ball);
+			}
+			},1000/settings.fps)
+	}
+	 
 	 
 	
 }
@@ -84,4 +112,69 @@ function changeDirection(x) {
 
 function getRadians(degree){
 	return (degree/180)*Math.PI;
+}
+function cleanArray(){
+	var newArray = [];
+	for(var i = 0;i<ballArray.length;i++){
+		if(ballArray[i].isAlive)
+			newArray.push(ballArray[i]);
+	}
+	ballArray = newArray;
+}
+
+function validateBallImpact(ball) {
+	var currentIndex = (ballArray.indexOf(ball));
+	for (var i = 0; i < ballArray.length; i++) {
+		var ball2 = ballArray[i];
+		if (i != currentIndex && ball2.isAlive && ball.isAlive && ball.timeLife>graceTime && ball2.timeLife>graceTime) {
+			var distance = Math.sqrt(Math.pow(ball2.initPos.x-ball.initPos.x,2)+(Math.pow(ball2.initPos.y-ball.initPos.y,2)));
+			
+			if(distance<((ball.radius+ball2.radius))){
+				
+				if((ball.speed*ball.radius)>(ball2.speed*ball2.radius)){
+					ball2.kill();
+					ball.speed+=0.1;
+					ball.radius+=(ball2.radius*0.2);
+					ball.opacity+=0.01;
+				}
+				else{
+					ball.kill();
+					ball2.speed+=0.1;
+					ball2.radius+=(ball.radius*0.2);
+					ball2.opacity+=0.01;
+				}
+					
+				cleanArray();
+			}
+			
+			/*if (ball.initPos.x + ball.radius < ball2.initPos.x - ball2.radius
+						&& ball.initPos.y + ball.radius < ball2.initPos.y - ball2.radius
+						&& ball.initPos.x < ball2.initPos.x && ball.initPos.y < ball2.initPos.y 
+						) {
+				ball2.kill();
+				cleanArray();}
+				*/
+			/* else if (ball.initPos.x - ball.radius < ball2.initPos.x + ball2.radius
+						&& ball.initPos.y - ball.radius < ball2.initPos.y + ball2.radius
+						&& ball.initPos.x > ball2.initPos.x && ball.initPos.y > ball2.initPos.y
+						) {
+				ball2.kill();
+				cleanArray();
+			}*/
+			/* else if (ball.initPos.x - ball.radius < ball2.initPos.x + ball2.radius
+						&& ball.initPos.y + ball.radius < ball2.initPos.y - ball2.radius
+						&& ball.initPos.x > ball2.initPos.x && ball.initPos.y < ball2.initPos.y
+						) {
+				ball2.kill();
+				cleanArray();
+			} else if (ball.initPos.x + ball.radius < ball2.initPos.x - ball2.radius
+						&& ball.initPos.y - ball.radius < ball2.initPos.y + ball2.radius
+						&& ball.initPos.x < ball2.initPos.x && ball.initPos.y > ball2.initPos.y
+						) {
+				ball2.kill();
+				cleanArray();
+			}
+			 	*/
+		}
+	}
 }
